2476d436a9c9990a5c5503f536023b15970d61b7
bernadlein <108117547+bernadlein@users.noreply.github.com>
2025-10-22 17:54:27 +0700

    Update schedule.ts

--- data/schedule.ts
+++ data/schedule.ts
@@ -12,7 +12,7 @@ export const weeklySailings: WeeklySailing[] = [
     id: "dlu-maumere",
     route: "Surabaya → Maumere",
     operator: "Darma Lautan Utama",
-    dateISO: "2025-10-31", // <-- UBAH SETIAP MINGGU
+    dateISO: "2025-11-1", // <-- UBAH SETIAP MINGGU
   },
 ];
 
@@ -22,6 +22,6 @@ export const weeklyClosing: WeeklySailing[] = [
     id: "closing-maumere",
     route: "Surabaya → Maumere",
     operator: "Darma Lautan Utama",
-    dateISO: "2025-10-30", // <-- UBAH JUGA JIKA PERLU
+    dateISO: "2025-10-31", // <-- UBAH JUGA JIKA PERLU
   },
 ];
