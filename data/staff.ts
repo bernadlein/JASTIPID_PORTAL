7f212f36c82d0f33e5a0edd8ec1d6183e9f26b55
bernadlein <108117547+bernadlein@users.noreply.github.com>
2025-12-09 15:32:30 +0700

    Update staff.ts

--- data/staff.ts
+++ data/staff.ts
@@ -9,25 +9,25 @@ export type Staff = {
 export const staff: Staff[] = [
   {
     name: "Bernad",
-    role: "CEO",
+    role: "Founder & Head of Operations Surabaya",
     phone: "6285338391953", // ganti ke nomor asli
     photo: "/Bernad.jpg",
   },
   {
     name: "Mega Lamawuran",
-    role: "CEO",
+    role: "Co-Founder & Head of Operations Flores",
     phone: "6282146791727",
     photo: "/mega.jpg",
   },
   {
     name: "Angelica Wulo",
-    role: "CEO",
+    role: "Area Coordinator Lembata",
     phone: "6281239885871",
     photo: "/angelica.jpg",
   },
   {
     name: "Dio Pratama",
-    role: "CEO",
+    role: "Area Coordinator Adonara",
     phone: "6282236042237",
     photo: "/dio.jpg",
   },
